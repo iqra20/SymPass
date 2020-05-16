bc36ce582ce60afe0cd782c6154b270be15bfa00
DavidB214 <dbarbera18@jaumebalmes.net>
2020-05-16 19:25:12 +0200

    added password list and add passwords

--- src/app/addpass/addpass.page.ts
+++ src/app/addpass/addpass.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-addpass',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addpass.page.scss'],
 })
 export class AddpassPage implements OnInit {
-
-  constructor() { }
+  title :string;
+  username :string;
+  password :string;
+  link :string;
+  description :string;
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-}
+
+  async anadirPass() {
+    fetch("http://3.124.237.156:8080/password/1", {
+      "method": "POST",
+      "headers": {
+        'Content-Type': 'application/json'
+      },
+      "body": JSON.stringify({
+        "title": this.title,
+        "username": this.username,
+        "password": this.password,
+        "description": this.description,
+        "link": this.link,
+        "type":3,
+        "icon": "correo.png"
+      })
+    })
+      .then(response => {
+        console.log(response);
+        this.router.navigate(['lista']);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+  }
--- src/app/lista/lista.page.ts
+++ src/app/lista/lista.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-lista',
@@ -8,11 +8,28 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ListaPage implements OnInit {
 
-
-  constructor() { 
-
+  public passwordList:any;
+  constructor(private router: Router) { 
+    this.getJson();
   }
   ngOnInit() {
+  }
+
+  thisPassword(password){
+    console.log(password);
+    let navigationExtras: NavigationExtras = {
+      state: {
+        parametros: password,
+      }
+    };
+    this.router.navigate(['detalle-pass'], navigationExtras);
+
+  }
+
+  async getJson(){
+//    const respuesta = await fetch("../../assets/test.json");
+const respuesta = await fetch("http://3.124.237.156:8080/password/1");
+this.passwordList= await respuesta.json();
   }
 
 }
