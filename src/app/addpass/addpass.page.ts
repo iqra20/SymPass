c35f17372b5f6faab9933b0994a062eef012c734
DavidB214 <dbarbera18@jaumebalmes.net>
2020-05-29 11:24:40 +0200

    edit pass+/generate password

--- src/app/addpass/addpass.page.ts
+++ src/app/addpass/addpass.page.ts
@@ -1,6 +1,6 @@
 import { ModalImgPage } from './../modal-img/modal-img.page';
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -9,18 +9,35 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./addpass.page.scss'],
 })
 export class AddpassPage implements OnInit {
+  seguridad:number;
   title :string;
   type:number;
+  api:"POST";
   username :string;
   password :string;
+  pass:any;
   link :string;
   description :string;
   icon:string="../../assets/icon/img.png";
+  color:string;
   constructor(private router: Router,private route: ActivatedRoute,private modalCtrl:ModalController) { 
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
-        this.id = this.router.getCurrentNavigation().extras.state.parametros;
+        this.id = this.router.getCurrentNavigation().extras.state.id;
+        this.pass=this.router.getCurrentNavigation().extras.state.parametros;
         console.log("en añadir pass estoy"+this.id);
+        console.log(this.pass);
+        if(this.pass){
+          this.title=this.pass.title;
+          this.type=this.pass.type;
+          this.username=this.pass.username;
+          this.password=this.pass.password;
+          this.link=this.pass.link;
+          this.description=this.pass.description;
+          this.icon=this.pass.icon;
+          this.api=this.router.getCurrentNavigation().extras.state.api;
+          this.id=this.pass.id;
+        }
       }
     });
   }
@@ -44,6 +61,11 @@ export class AddpassPage implements OnInit {
       this.password+=this.caracteres.charAt(Math.floor(Math.random()*this.caracteres.length));
     }
     console.log("pass aleatoria= "+this.password)
+    console.log("seguridad="+this.comprobarPassword());
+    if(this.comprobarPassword()<10){
+      console.log("falta seguridad..")
+      this.generatePass();
+    }
   }
 
   async openModal(){
@@ -62,9 +84,11 @@ export class AddpassPage implements OnInit {
     
   }
 
-  async anadirPass() {
+ async anadirPass() {
+   console.log("añadir pas");
+   console.log(this.api);    
     fetch("http://3.124.237.156:8080/password/"+this.id, {
-      "method": "POST",
+      "method": this.api,
       "headers": {
         'Content-Type': 'application/json'
       },
@@ -85,5 +109,87 @@ export class AddpassPage implements OnInit {
       .catch(err => {
         console.log(err);
       });
+    
+    
     }
+    onKey(newValue) {
+      this.seguridad=this.comprobarPassword();
+      console.log("seguridad de tu pass");
+      if(this.seguridad>9){
+        console.log("pass fuerte");
+      }else{
+        if(this.seguridad>5){
+          console.log("pass mediana")
+        }else{
+          console.log("pass debil")
+        }
+      }
+  }
+    comprobarPassword():number{
+      this.seguridad = 0;
+      if (this.password.length!=0){
+        if(this.tiene_minusculas&&this.tiene_majusculas()&&this.tiene_numeros()&&this.tiene_simbolos()){
+          this.seguridad+=7;
+        }else{
+          if (this.tiene_numeros() && this.tiene_minusculas()||this.tiene_majusculas){
+            this.seguridad += 2;
+         }
+         if (this.tiene_minusculas() && this.tiene_majusculas()){
+            this.seguridad += 2;
+         }
+         if(this.tiene_simbolos()){
+           this.seguridad+=2;
+         }
+        }
+        if(this.password.length<4){
+          this.seguridad=0;
+        }else{
+          if (this.password.length >= 4 && this.password.length <= 5){
+            this.seguridad += 2;
+           }else{
+              if (this.password.length >= 6 && this.password.length <= 8){
+                this.seguridad += 3;
+              }else{
+                 if (this.password.length > 8){
+                    this.seguridad += 5;
+                 }
+              }
+           }
+        }   
+      }  
+      return this.seguridad;        
+   }   
+   tiene_minusculas():boolean{
+     for(let i=0;i<this.password.length;i++){
+       if(this.password.charAt(i)>='a'&& this.password.charAt(i)<='z'){
+        return true;
+       }
+     }
+     return false;
+   }
+   tiene_majusculas():boolean{
+    for(let i=0;i<this.password.length;i++){
+      if(this.password.charAt(i)>='A'&& this.password.charAt(i)<='Z'){
+       return true;
+      }
+    }
+    return false;
+   }
+   tiene_numeros():boolean{
+    for(let i=0;i<this.password.length;i++){
+      if(this.password.charAt(i)>='0'&& this.password.charAt(i)<='9'){
+       return true;
+      }
+    }
+    return false;
+   }
+   tiene_simbolos():boolean{
+     let simbolos:any[]=['!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','[','}','}',';',':','<',',','>','.','?','/'];
+    for(let i=0;i<this.password.length;i++){
+      if(simbolos.indexOf(this.password.charAt(i)!)!=-1){
+       return true;
+      }
+    }
+    return false;
+   }
   }
